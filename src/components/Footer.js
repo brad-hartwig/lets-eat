0d44a610c7d7400e7d8a4d1454b1bb353034ae62
brad-hartwig <hartwig.brad@gmail.com>
2021-12-30 11:03:55 -0600

    copyright change; ghpages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='py-3 bg-dark'>
       <div className='container'>
-        <p className='m-0 text-center text-white'>Copyright &copy; Let&rsquo;s Eat! {new Date().getFullYear()}</p>
+        <p className='m-0 text-center text-white'>Copyright &copy; LetsEat {new Date().getFullYear()}</p>
       </div>
     </footer>
   );
