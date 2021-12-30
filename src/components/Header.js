3ce5eb6fbac50e11b0f248064b4b8e8858ff2fc9
brad-hartwig <hartwig.brad@gmail.com>
2021-12-30 10:45:42 -0600

    footer now relative; header height reduced; images moved to /assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ const pizzaStyle = {
 const Header = () => {
   return (
     <header className='bg-dark py-1' style={pizzaStyle}>
-      <div className='container px-4 px-lg-5 my-4'>
+      <div className='container my-1'>
         <div className='text-center text-black'>
-          <h1 className='display-4 fw-bolder header-title'>Let&rsquo;s Eat!</h1>
-          <p className='lead fw-normal text-black-50 mb-0'>让我们吃 &#8226; Comamos &#8226; चलो खाते हैं &#8226; Давайте есть &#8226; Mangeons &#8226; Mangiamo</p>
+          <h1 className='header-title display-4 fw-bolder mb-0'>Let&rsquo;s Eat!</h1>
+          <p className='header-subtitle lead fw-normal text-black-50 mb-0'>让我们吃 &#8226; Comamos &#8226; चलो खाते हैं &#8226; Давайте есть &#8226; Mangeons &#8226; Mangiamo</p>
         </div>
       </div>
     </header>
--- src/data/images.js
+++ src/data/images.js
@@ -1,18 +1,18 @@
-import brussel_chips from 'images/brussel_chips.jpg';
-import brussel_chips_m from 'images/brussel_chips_m.jpg';
-import brussel_chips_s from 'images/brussel_chips_s.jpg';
-import italian_meatballs from 'images/italian_meatballs.jpg';
-import italian_meatballs_m from 'images/italian_meatballs_m.jpg';
-import italian_meatballs_s from 'images/italian_meatballs_s.jpg';
-import kielbasa_skillet from 'images/kielbasa_skillet.jpg';
-import kielbasa_skillet_m from 'images/kielbasa_skillet_m.jpg';
-import kielbasa_skillet_s from 'images/kielbasa_skillet_s.jpg';
-import pancake_mountain from 'images/pancake_mountain.jpg';
-import pancake_mountain_m from 'images/pancake_mountain_m.jpg';
-import pancake_mountain_s from 'images/pancake_mountain_s.jpg';
-import queso_brat_scramble from 'images/queso_brat_scramble.jpg';
-import queso_brat_scramble_m from 'images/queso_brat_scramble_m.jpg';
-import queso_brat_scramble_s from 'images/queso_brat_scramble_s.jpg';
+import brussel_chips from 'assets/brussel_chips.jpg';
+import brussel_chips_m from 'assets/brussel_chips_m.jpg';
+import brussel_chips_s from 'assets/brussel_chips_s.jpg';
+import italian_meatballs from 'assets/italian_meatballs.jpg';
+import italian_meatballs_m from 'assets/italian_meatballs_m.jpg';
+import italian_meatballs_s from 'assets/italian_meatballs_s.jpg';
+import kielbasa_skillet from 'assets/kielbasa_skillet.jpg';
+import kielbasa_skillet_m from 'assets/kielbasa_skillet_m.jpg';
+import kielbasa_skillet_s from 'assets/kielbasa_skillet_s.jpg';
+import pancake_mountain from 'assets/pancake_mountain.jpg';
+import pancake_mountain_m from 'assets/pancake_mountain_m.jpg';
+import pancake_mountain_s from 'assets/pancake_mountain_s.jpg';
+import queso_brat_scramble from 'assets/queso_brat_scramble.jpg';
+import queso_brat_scramble_m from 'assets/queso_brat_scramble_m.jpg';
+import queso_brat_scramble_s from 'assets/queso_brat_scramble_s.jpg';
 
 export default{
   brussel_chips,
